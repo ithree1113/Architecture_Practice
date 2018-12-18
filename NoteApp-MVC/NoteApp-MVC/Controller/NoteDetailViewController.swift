d393123d43b6ee56af730c403e50e9394f591d35
Eddie <ithree1113@gmail.com>
2018-12-18 22:12:38 +0800

    [Add] 儲存照片於sandbox

--- NoteApp-MVC/NoteApp-MVC/Controller/NoteDetailViewController.swift
+++ NoteApp-MVC/NoteApp-MVC/Controller/NoteDetailViewController.swift
@@ -22,26 +22,30 @@ class NoteDetailViewController: UIViewController {
         
         detailTextView.text = note?.text
         
-        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(addImage(_:)))
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(cameraBtnDidTap(_:)))
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done(_:)))
         
         navigationItem.rightBarButtonItems = [doneButton, cameraButton]
     }
     
+    
+    // MARK: - Selector
     @objc func done(_ sender: UIBarButtonItem) {
         note?.text = detailTextView.text
-        if isNewImage {
-            let uuid = UUID()
-            note?.imageName = String(format: "%@.jpg", uuid.uuidString)
+        if isNewImage, let photoFolderURL = getPhotoFolder() {
+            saveImageTo(url: photoFolderURL)
         }
         didFinishUpdate?(note!)
         navigationController?.popViewController(animated: true)
     }
     
-    @objc func addImage(_ sender: UIBarButtonItem) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.sourceType = .savedPhotosAlbum
-        imagePicker.delegate = self
+    @objc func cameraBtnDidTap(_ sender: UIBarButtonItem) {
+        let imagePicker: UIImagePickerController = {
+            let picker = UIImagePickerController()
+            picker.sourceType = .savedPhotosAlbum
+            picker.delegate = self
+            return picker
+        }()
         present(imagePicker, animated: true, completion: nil)
     }
 }
@@ -56,4 +60,43 @@ extension NoteDetailViewController: UIImagePickerControllerDelegate, UINavigatio
         dismiss(animated: true, completion: nil)
     }
     
+}
+
+
+// MARK: - Subroutine
+extension NoteDetailViewController {
+    func getPhotoFolder() -> URL? {
+        let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let photoDirectoryURL = documentURL.appendingPathComponent("Photo", isDirectory: true)
+        
+        if !FileManager.default.fileExists(atPath: photoDirectoryURL.path) {
+            do {
+                try FileManager.default.createDirectory(at: photoDirectoryURL, withIntermediateDirectories: false, attributes: nil)
+                return photoDirectoryURL
+            } catch {
+                print(error)
+                return nil
+            }
+        } else {
+            return photoDirectoryURL
+        }
+    }
+    
+    func saveImageTo(url: URL) {
+        let uuid = UUID()
+        let imageName = String(format: "%@.jpg", uuid.uuidString)
+        let imageURL = url.appendingPathComponent(imageName)
+        let imageData = detailImageView.image!.jpegData(compressionQuality: 1)
+        
+        do {
+            try imageData?.write(to: imageURL)
+            if let oldImageName = note?.imageName, !oldImageName.isEmpty {
+                let oldImageURL = url.appendingPathComponent(oldImageName)
+                try FileManager.default.removeItem(at: oldImageURL)
+            }
+            note?.imageName = imageName
+        } catch {
+            print(error)
+        }
+    }
 }
